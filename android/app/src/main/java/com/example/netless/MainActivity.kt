14a4f851fce2b31a8f3a4812931cfb3e1b6793bf
Diego <34724728+Calcoph@users.noreply.github.com>
2024-01-30 19:54:48 +0100

    interfaz básica android

--- android/app/src/main/java/com/example/netless/MainActivity.kt
+++ android/app/src/main/java/com/example/netless/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.netless
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.OnClickListener
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+        val button = findViewById<Button>(R.id.button2);
+
+        button.setOnClickListener {
+            val intent = Intent(this, Chat::class.java);
+            startActivity(intent);
+        }
     }
 }
