c3866e9b47a8ef4f63e1262ca799d33801c65b9b
Diego <34724728+Calcoph@users.noreply.github.com>
2024-04-15 15:44:58 +0200

    (windows) (comunicación) añadir tipo Identificación

--- windows/comunicacion.py
+++ windows/comunicacion.py
@@ -8,10 +8,11 @@ class TipoMensaje(Enum):
     TEXTO = 0x00
     FICHERO = 0x01
     CONTINUACION_FICHERO = 0x02
+    IDENTIFICACION = 0x03
 
 class CabeceraFichero:
     TAMAÑO_MINIMO: int = 4
-    def __init__(self, data: bytes, metadata: str) -> None:
+    def __init__(self, data: bytes | None, metadata: str) -> None:
         self.data = data
         self.metadata = metadata
     
@@ -27,7 +28,7 @@ class CabeceraFichero:
 
 class Cabecera:
     TAMAÑO: int = 5
-    def __init__(self, data: bytes, type: TipoMensaje, message_size: int) -> None:
+    def __init__(self, data: bytes | None, type: TipoMensaje, message_size: int) -> None:
         self.data = data
         self.type = type
         self.message_size = message_size
@@ -41,3 +42,29 @@ class Cabecera:
         # Tamaño máximo de mensaje: casi 4 GiB
         cursor += 4
         return Cabecera(data, type, message_size)
+
+class Identificacion:
+    def __init__(self, data: bytes | None, alias: str, id: str) -> None:
+        self.alias = str
+        self.id = str
+
+    def from_bytes(data: bytes) -> Identificacion:
+        cursor = 0
+        tamaño_alias = data[cursor:cursor+2] # 2 bytes: tamaño del alias
+        cursor += 2
+        tamaño_id = data[cursor:cursor+2] # 2 bytes: tamaño del id
+        cursor += 2
+        alias = data[cursor:cursor+tamaño_alias].decode("utf-8")
+        cursor += tamaño_alias
+        id = data[cursor:cursor+tamaño_id].decode("utf-8")
+        cursor += tamaño_id
+
+        return Identificacion(data, alias, id)
+    
+    def to_bytes(self) -> bytes:
+        tamaño_alias = len(self.alias).to_bytes(2)
+        tamaño_id = len(self.id).to_bytes(2)
+        alias = self.alias.encode("utf-8")
+        id = self.id.encode("utf-8")
+
+        return tamaño_alias + tamaño_id + alias + id
