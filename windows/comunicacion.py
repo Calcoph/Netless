7153166a4a2ecd61bf189386445fb02664e824cf
Diego <34724728+Calcoph@users.noreply.github.com>
2024-04-15 15:57:01 +0200

    (windows) ampliar protocolo comunicación

--- windows/comunicacion.py
+++ windows/comunicacion.py
@@ -9,6 +9,7 @@ class TipoMensaje(Enum):
     FICHERO = 0x01
     CONTINUACION_FICHERO = 0x02
     IDENTIFICACION = 0x03
+    PEDIR_IDENTIFICACION = 0x04
 
 class CabeceraFichero:
     TAMAÑO_MINIMO: int = 4
@@ -25,6 +26,12 @@ class CabeceraFichero:
         metadata = conn.recv(metadata_size).decode("utf-8")
 
         return CabeceraFichero(data, metadata_size, metadata)
+    
+    def to_bytes(self) -> bytes:
+        metadata_size = len(self.metadata).to_bytes(4)
+        metadata = self.metadata.encode("utf-8")
+
+        return metadata_size + metadata
 
 class Cabecera:
     TAMAÑO: int = 5
@@ -42,6 +49,12 @@ class Cabecera:
         # Tamaño máximo de mensaje: casi 4 GiB
         cursor += 4
         return Cabecera(data, type, message_size)
+
+    def to_bytes(self) -> bytes:
+        type = self.type.to_bytes(1)
+        message_size = self.message_size.to_bytes(4)
+
+        return type + message_size
 
 class Identificacion:
     def __init__(self, data: bytes | None, alias: str, id: str) -> None:
@@ -68,3 +81,24 @@ class Identificacion:
         id = self.id.encode("utf-8")
 
         return tamaño_alias + tamaño_id + alias + id
+
+    def bytes_con_cabecera(self) -> bytes:
+        auto_bytes = self.to_bytes()
+        cabecera = Cabecera(None, TipoMensaje.IDENTIFICACION, len(auto_bytes)).to_bytes()
+
+        return cabecera + auto_bytes
+
+class PedirIdentificacion:
+    def __init__(self) -> None:
+        pass
+
+    def from_bytes(data: bytes) -> PedirIdentificacion:
+        if len(data) > 0:
+            raise Exception("PedirIdentificación es un mensaje de tamaño 0")
+        return PedirIdentificacion()
+
+    def to_bytes(self) -> bytes:
+        return bytes()
+    
+    def butes_con_cabecera(self) -> bytes:
+        return Cabecera(None, TipoMensaje.PEDIR_IDENTIFICACION, 0).to_bytes()
